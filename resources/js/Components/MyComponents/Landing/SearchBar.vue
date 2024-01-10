b4dd54b313d4c4f33042ebe37a0a9ccdaeb622f2
Miguel Vázquez <miguelvz26.mv@gmail.com>
2024-01-09 22:50:41 -0600

    landing layout

---
+++ resources/js/Components/MyComponents/Landing/SearchBar.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="w-1/3 border rounded-[3px] flex items-center py-0">
+        <span class="border-r px-3 text-gray1 py-3">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                class="size-4">
+                <path stroke-linecap="round" stroke-linejoin="round"
+                    d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+            </svg>
+        </span>
+        <input type="text" placeholder="¿Qué estás buscando?" class="placeholder:text-gray1 px-3 py-1 border-0 w-full mr-2 focus:ring-0">
+    </div>
+</template>
+<script>
+export default {
+
+}
+</script>
---
+++ resources/js/Layouts/LandingLayout.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+
+        <Head :title="title" />
+
+        <div class="min-h-screen bg-white dark:bg-gray-900">
+            <nav class="bg-white dark:bg-gray-800 border-b border-secondary dark:border-gray-700">
+                <div class="flex items-center justify-between h-20 px-8">
+                    <Link :href="route('dashboard')">
+                        <ApplicationMark class="block w-36" />
+                    </Link>
+                    <SearchBar />
+                    <Nav />
+                </div>
+            </nav>
+            <main>
+                <slot />
+            </main>
+        </div>
+    </div>
+</template>
+<script>
+import { Head, Link } from '@inertiajs/vue3';
+import ApplicationMark from '@/Components/ApplicationMark.vue';
+import SearchBar from '@/Components/MyComponents/Landing/SearchBar.vue';
+import Nav from '@/Components/MyComponents/Landing/Nav.vue';
+
+export default {
+    components: {
+        Head,
+        Link,
+        ApplicationMark,
+        SearchBar,
+        Nav,
+    },
+    props: {
+        title: String,
+    }
+}
+</script>
