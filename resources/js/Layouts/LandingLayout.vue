8f7b7f09b7e472608bb40c74240ce7482cdb15cb
Miguel Vázquez <miguelvz26.mv@gmail.com>
2024-01-11 09:39:11 -0600

    footer

--- resources/js/Layouts/LandingLayout.vue
+++ resources/js/Layouts/LandingLayout.vue
@@ -9,7 +9,7 @@
                 <div class="mx-auto px-4 sm:px-6 lg:px-10">
                     <div class="flex justify-between items-center h-20">
                         <!-- Logo -->
-                        <ApplicationMark class="block w-1/3 sm:w-1/12" />
+                        <ApplicationMark class="w-1/3 sm:w-1/12" />
                         <!-- barra de busqueda -->
                         <SearchBar class="hidden sm:flex" />
                         <!-- navegacion -->
@@ -41,14 +41,59 @@
             </nav>
             <header class="hidden sm:block mx-56 mt-5">
                 <ul class="flex items-center justify-between w-full">
-                    <li v-for="(item, index) in categories" :key="index" class="border-b-2 border-transparent hover:border-primary">
-                        <Link :href="route('landing.products', {filter: item})">{{ item }}</Link>
+                    <li v-for="(item, index) in categories" :key="index"
+                        class="border-b-2 border-transparent hover:border-primary">
+                        <Link :href="route('landing.products', { filter: item })">{{ item }}</Link>
                     </li>
                 </ul>
             </header>
             <main>
                 <slot />
             </main>
+            <footer class="mt-7 border-t border-secondary pt-3">
+                <section class="px-3 lg:grid grid-cols-4 gap-x-4">
+                    <div>
+                        <ApplicationMark class="block w-1/3 sm:w-1/3" />
+                        <small>Elevando elegancia, definiendo estilo.</small>
+                    </div>
+                    <div class="mt-2">
+                        <h2>Categorías</h2>
+                        <ul class="flex flex-col">
+                            <Link :href="route('landing.products', { filter: item })" v-for="(item, index) in categories"
+                                :key="index" class="text-xs self-start border-b border-transparent hover:border-primary">{{ item }}</Link>
+                        </ul>
+                    </div>
+                    <div class="mt-2">
+                        <h2>Contáctanos</h2>
+                        <p class="flex items-center space-x-2 text-xs">
+                            <span>3310223000</span>
+                            <a href="https://api.whatsapp.com/send?phone=523310223000&text=Hola!%20vi%20tu%20página%20,%20me%20interesan%20sus%20productos!"
+                                target="_blank"
+                                class="bg-black text-white rounded-full size-4 flex items-center justify-center">
+                                <i class="fa-brands fa-whatsapp text-xs"></i>
+                            </a>
+                        </p>
+                    </div>
+                    <div class="mt-2">
+                        <h2>Encuentranos en nuestras redes sociales</h2>
+                        <ul class="flex items-center space-x-2">
+                            <a href="#">
+                                <i class="fa-brands fa-facebook"></i>
+                            </a>
+                            <a href="#">
+                                <i class="fa-brands fa-square-instagram"></i>
+                            </a>
+                        </ul>
+                    </div>
+                </section>
+                <section class="py-2 border-y border-secondary flex items-center justify-between px-3 text-[10px] sm:text-sm">
+                    <p class="text-gray1">&copy; 2024 • Nala.</p>
+                    <a href="https://dtw.com.mx" target="_blank" class="flex items-center space-x-2">
+                        <small>by Digtital Tech Work</small>
+                        <DTWMark class="w-5" />
+                    </a>
+                </section>
+            </footer>
         </div>
     </div>
 </template>
@@ -58,6 +103,7 @@ import ApplicationMark from '@/Components/ApplicationMark.vue';
 import Dropdown from '@/Components/Dropdown.vue';
 import DropdownLink from '@/Components/DropdownLink.vue';
 import SearchBar from '@/Components/MyComponents/Landing/SearchBar.vue';
+import DTWMark from '@/Components/MyComponents/Layout/DTWMark.vue';
 import Nav from '@/Components/MyComponents/Landing/Nav.vue';
 
 export default {
@@ -100,6 +146,7 @@ export default {
         Nav,
         Dropdown,
         DropdownLink,
+        DTWMark,
     },
     props: {
         title: String,
