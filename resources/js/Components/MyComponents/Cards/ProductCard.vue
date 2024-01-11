9c453fe5cd1fcff4dc231a374903b0bf5711de2a
Miguel Vázquez <miguelvz26.mv@gmail.com>
2024-01-10 20:59:17 -0600

    banner & gallery

--- resources/js/Components/MyComponents/Cards/ProductCard.vue
+++ resources/js/Components/MyComponents/Cards/ProductCard.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="border rounded-[3px] py-3 border-secondary h-64 lg:h-80 lg:w-11/12 flex flex-col justify-center">
-        <figure class="h-2/3 bg-secondary">
+        <figure class="h-2/3 flex items-center justify-center">
+            <img src="@/../../public/images/jewerly.png" class="object-cover">
         </figure>
         <main class="mt-3">
             <p class="text-center">Pulsera Pantera Oro amarillo 14K Zirconias blancas y verdes {{ product }}</p>
--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,52 +1,56 @@
 <template>
     <LandingLayout title="Inicio">
         <!-- banner -->
-        <section class="h-36 bg-secondary mt-2"></section>
+        <section class="h-28 lg:h-64 mt-2">
+            <Carousel v-bind="bannerSettings" :itemsToShow="1">
+                <Slide v-for="slide in 3" :key="slide">
+                    <img src="@/../../public/images/banner1.png" class="h-full object-contain">
+                </Slide>
+                <template #addons>
+                    <Pagination />
+                </template>
+            </Carousel>
+        </section>
 
         <!-- productos populares -->
         <section>
-            <h1 class="flex items-center space-x-2 text-lg mx-3 mt-5">
+            <h1 class="flex items-center space-x-2 text-lg py-2 mx-3 mt-5 lg:mt-12">
                 <span>Productos populares</span>
-                <PrimaryButton class="!py-1 !px-2">Ver todos</PrimaryButton>
+                <Link :href="route('landing.products', {filters: 'Todos'})">
+                    <PrimaryButton class="!py-1 !px-2">Ver todos</PrimaryButton>
+                </Link>
             </h1>
 
             <article class="items-center rounded-[3px] mt-3 py-2 px-3 lg:px-14 active:cursor-grabbing">
-                <!-- <button @click="navigateCarousel(-1)"
-                    class="flex items-center justify-center size-5 rounded-full hover:bg-black hover:text-white">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                        stroke="currentColor" class="size-3">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
-                    </svg>
-                </button> -->
-
-                <!-- <button @click="navigateCarousel(1)"
-                    class="flex items-center justify-center size-5 rounded-full hover:bg-black hover:text-white">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                        stroke="currentColor" class="size-3">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
-                    </svg>
-                </button> -->
-                <Carousel v-bind="settings" :breakpoints="breakpoints">
+                <Carousel v-bind="popularSettings" :breakpoints="breakpoints">
                     <Slide v-for="slide in popularProducts" :key="slide">
-                        <!-- <div class="carousel__item">{{ slide }}</div> -->
                         <ProductCard :product="slide" />
                     </Slide>
-
                     <template #addons>
                         <Navigation />
                     </template>
                 </Carousel>
-                <!-- <div class="flex justify-center space-x-2 mt-2">
-                    <button v-for="item in 10" :key="item" @click="currentPopularProduct = item"
-                        class="w-2 h-2 rounded-full"
-                        :class="currentPopularProduct == item ? 'bg-secondary' : 'bg-gray4'"></button>
-                </div> -->
             </article>
         </section>
+        
+        <!-- Galeria -->
+        <section>
+            <h1 class="text-lg mx-3 mt-5">
+                <span>Galería</span>
+            </h1>
 
+            <article class="items-center rounded-[3px] mt-1 py-1 px-3 lg:px-14 active:cursor-grabbing">
+                <Carousel v-bind="galerySettings" :breakpoints="breakpoints">
+                    <Slide v-for="slide in 20" :key="slide">
+                        <img src="@/../../public/images/galery1.png" class="w-full">
+                    </Slide>
+                </Carousel>
+            </article>
+        </section>
     </LandingLayout>
 </template>
 <script>
+import { Link } from '@inertiajs/vue3';
 import LandingLayout from '@/Layouts/LandingLayout.vue';
 import PrimaryButton from '@/Components/PrimaryButton.vue';
 import ProductCard from '@/Components/MyComponents/Cards/ProductCard.vue';
@@ -56,13 +60,24 @@ import 'vue3-carousel/dist/carousel.css';
 export default {
     data() {
         return {
-            currentPopularProduct: 0,
             popularProducts: [1,2,3,4,5,6,7,8,9,10],
-            settings: {
+            popularSettings: {
                 wrapAround: true,
                 itemsToShow: 2,
                 snapAlign: 'start',
                 autoplay: 5000,
+            },
+            galerySettings: {
+                wrapAround: true,
+                itemsToShow: 2.7,
+                snapAlign: 'start',
+                autoplay: 7000,
+            },
+            bannerSettings: {
+                wrapAround: true,
+                itemsToShow: 1,
+                snapAlign: 'start',
+                autoplay: 10000,
             },
             // breakpoints are mobile first
             // any settings not specified will fallback to the carousel settings
@@ -88,7 +103,8 @@ export default {
         Slide,
         Pagination,
         Navigation,
-        ProductCard
+        ProductCard,
+        Link,
     },
 }
 </script>
